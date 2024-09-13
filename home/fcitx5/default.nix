b488ced739c7526153883ea39e466eb01c5d4324
Fython <fang.jia.hui123@gmail.com>
2024-09-13 22:06:55 +0800

    make fcitx5 rime beauty

--- home/fcitx5/default.nix
+++ home/fcitx5/default.nix
@@ -1,23 +1,37 @@
-{ config, pkgs-unstable, ... }:
+{ pkgs-unstable, ... }:
 
 {
+  # themes
+  home.file.".local/share/fcitx5/themes" = {
+    source = ./themes;
+    recursive = true;
+  };
+  xdg.configFile = {
+    "fcitx5/profile" = {
+      source = ./profile;
+      # every time fcitx5 switch input method, it will modify ~/.config/fcitx5/profile,
+      # so we need to force replace it in every rebuild to avoid file conflict.
+      force = true;
+    };
+    # apply themes
+    "fcitx5/conf/classicui.conf".source = ./conf/classicui.conf;
+    "fcitx5/conf/rime.conf".source = ./conf/rime.conf;
+  };
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = let
       # fcitx5 rime input method
       local-fcitx5-rime = (pkgs-unstable.fcitx5-rime.override {
         # 引入自定义的配置
-        rimeDataPkgs = [
-          ./rime-config
-        ];
+        rimeDataPkgs = [ ./rime-config ];
       }).overrideAttrs (final: prev: {
-          # 支持 lua 脚本
-          buildInputs = [ pkgs-unstable.fcitx5 pkgs-unstable.librime ];
-        });
-    in with pkgs-unstable; [ 
-        local-fcitx5-rime 
-        fcitx5-configtool 
-        fcitx5-chinese-addons 
-      ];
+        # 支持 lua 脚本
+        buildInputs = [ pkgs-unstable.fcitx5 pkgs-unstable.librime ];
+      });
+    in with pkgs-unstable; [
+      local-fcitx5-rime
+      fcitx5-configtool
+      fcitx5-chinese-addons
+    ];
   };
 }
