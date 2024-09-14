78df8ba00fc3129716ae78dda0c72cbdf049a121
vm <vm@example.com>
2024-09-14 18:32:06 +0800

    add apple sf mono fonts

--- home/local-fonts/default.nix
+++ home/local-fonts/default.nix
@@ -3,6 +3,7 @@ let
   fontPaths = [
     ./monaco # 苹果 Monaco 字体
     ./menlo # 苹果 Menlo 字体
+    ./sf_mono # 苹果 SF Mono 字体
     ./MiSans_L3 # 小米生僻字
     ./HarmonyOS_Sans_TC # 华为鸿蒙字体（繁体）
     ./HarmonyOS_Sans_SC # 华为鸿蒙字体（简体）
