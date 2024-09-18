9706468e32f1f6b666fa754b0ad1aafc8afb00a1
Fython <fang.jia.hui123@gmail.com>
2024-09-18 23:25:45 +0800

    add program visidata to view csv use vd command

--- home/programs/python/default.nix
+++ home/programs/python/default.nix
@@ -2,6 +2,9 @@
   # Python 
   home.packages = with pkgs; [
     (python311.withPackages (pyPkgs: with pyPkgs; [ ipython requests ]))
+    # view csv xls in terminal
+    visidata
+    # run python applications in isolated environments 
     pipx
   ];
 }
