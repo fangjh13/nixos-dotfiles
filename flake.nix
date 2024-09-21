bc6344b52d4d6a12f47acb2e2b358a70481bf950
Fython <fang.jia.hui123@gmail.com>
2024-09-21 18:04:01 +0800

    add comment

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Fython's NixOS Configuration";
 
   inputs = {
+    # https://nixos.wiki/wiki/FAQ#What_are_channels_and_how_do_they_get_updated?
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
