efc18311eac11a7edd2a527d5390e3369085609d
Fython <fang.jia.hui123@gmail.com>
2024-09-30 11:46:55 +0800

    update wezterm config

--- home/shell/terminals/wezterm/default.nix
+++ home/shell/terminals/wezterm/default.nix
@@ -4,5 +4,6 @@
     # FIXME: temporarily disable because of neovim in tmux open terminal print chao message
     # https://github.com/wez/wezterm/issues/5007
     enableZshIntegration = false;
+    extraConfig = builtins.readFile ./wezterm.lua;
   };
 }
