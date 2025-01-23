7a27debb2c689d09d532cacb3f6e41e84fad645a
Fython <fang.jia.hui123@gmail.com>
2025-01-23 15:41:53 +0800

    imv config

--- home/apps/media.nix
+++ home/apps/media.nix
@@ -5,8 +5,6 @@
     # audio control
     pavucontrol
     playerctl
-    # images
-    imv
   ];
 
   programs = {
@@ -17,6 +15,70 @@
     };
 
     obs-studio.enable = true;
+
+    imv = {
+      enable = true;
+      # https://man.archlinux.org/man/imv.1.en
+      settings = {
+        # Define some key bindings
+        binds = {
+          q = "quit";
+          # copy file path
+          w = "exec echo $imv_current_file | wl-copy";
+          # copy image file
+          yy = "exec cat $imv_current_file | wl-copy";
+
+          # Image navigation
+          "<Left>" = "prev";
+          "<bracketleft>" = "prev";
+          "<Right>" = "next";
+          "<bracketright>" = "next";
+          "<Ctrl+h>" = "prev";
+          "<Ctrl+l>" = "next";
+          gg = "goto 1";
+          "<Shift+G>" = "goto -1";
+
+          # Panning
+          j = "pan 0 -50";
+          k = "pan 0 50";
+          h = "pan 50 0";
+          l = "pan -50 0";
+
+          # Zooming
+          "<Up>" = "zoom 1";
+          "<Shift+plus>" = "zoom 1";
+          i = "zoom 1";
+          "<Down>" = "zoom -1";
+          "<minus>" = "zoom -1";
+          o = "zoom -1";
+          "<Ctrl+0>" = "zoom actual";
+          "<Ctrl+equal>" = "zoom 10";
+          "<Ctrl+minus>" = "zoom -10";
+
+          # Rotate Clockwise by 90 degrees
+          "<Ctrl+r>" = "rotate by 90";
+
+          # Other commands
+          x = "close";
+          f = "fullscreen";
+          d = "overlay";
+          p = "exec echo $imv_current_file";
+          c = "center";
+          s = "scaling next";
+          "<Shift+S>" = "upscaling next";
+          a = "zoom actual";
+          r = "reset";
+
+          # Gif playback
+          "<period>" = "next_frame";
+          "<space>" = "toggle_playing";
+
+          # Slideshow control
+          t = "slideshow +1";
+          "<Shift+T>" = "slideshow -1";
+        };
+      };
+    };
   };
 
   services = { playerctld.enable = true; };
