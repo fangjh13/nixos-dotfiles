cc028f488b4263551d49c37d7f2308edb253c41f
Fython <fang.jia.hui123@gmail.com>
2025-03-07 15:07:55 +0800

    fit ser7

--- home/scripts/cpu-temp.nix
+++ home/scripts/cpu-temp.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "cpu-temp" ''
+  # intel cpu
   temp=$(sensors | grep 'Package id 0:' | awk '{print $4}' | tr -d '+°C')
+  # amd cpu
+  if [ -z "$temp" ]; then
+      temp=$(sensors | grep 'Tctl:' | awk '{print $2}' | tr -d '+°C')
+  fi
   temp=''${temp%.*}
   echo $temp
 ''
