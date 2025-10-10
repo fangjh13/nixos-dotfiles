a6eb163ba628b168afd32e9b0b6a5b618e853c64
Fython <fang.jia.hui123@gmail.com>
2025-10-10 22:54:51 +0800

    ✨ feat(tmux-plugins): add tmux-resurrect plugin configuration
    - configure resurrect_dir to follow XDG Base Directory Specification
    - set save/restore keys to 'S' and 'R'
    - enable capturing pane contents for better session restoration
    - add neovim and vim specific strategies for restoring editor sessions
    - include a post-save hook to fix neovim session restoration issues
    
    ♻️ refactor(tmux-config): update tmux config reload keybinding and message
    
    - change reload key from 'R' to 'C-r' for better accessibility

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   lib,
+  config,
   ...
 }: {
   home.file.".config/tmux/themepack" = {
@@ -21,6 +22,23 @@
         #     set -g @dracula-refresh-rate 100
         #   '';
         # }
+        {
+          plugin = resurrect;
+          extraConfig = ''
+            resurrect_dir="${config.xdg.stateHome}/tmux-ressurect"
+            set -g @resurrect-dir $resurrect_dir
+            set -g @resurrect-save 'S'
+            set -g @resurrect-restore 'R'
+            set -g @resurrect-capture-pane-contents 'on'
+            # for vim
+            set -g @resurrect-strategy-vim 'session'
+            # for neovim
+            set -g @resurrect-strategy-nvim 'session'
+            # fix tmux-resurrect in neovim
+            # https://discourse.nixos.org/t/how-to-get-tmux-resurrect-to-restore-neovim-sessions/30819
+            set -g @resurrect-hook-post-save-all "sed -E -i 's| --cmd .*-vim-pack-dir||g; s|/nix/store[^ ]+bin/||g; s| --cmd lua||g; s|/etc/profiles/per-user/$USER/bin/||g; s|vim.g[^ ]+nvim-ruby[^ ]+||g' $(readlink -f $resurrect_dir/last)"
+          '';
+        }
         {
           plugin = tmux-floax;
           extraConfig = ''
