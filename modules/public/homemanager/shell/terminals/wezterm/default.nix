037f8740ec71ef61725717fc17079f37d4724f62
Fython <fang.jia.hui123@gmail.com>
2026-04-07 23:44:09 +0800

    update mac config

--- modules/public/homemanager/shell/terminals/wezterm/default.nix
+++ modules/public/homemanager/shell/terminals/wezterm/default.nix
@@ -1,7 +1,6 @@
 {pkgs-unstable, ...}: {
   programs.wezterm = {
     enable = true;
-    package = pkgs-unstable.wezterm;
     # FIXME: temporarily disable because of neovim in tmux open terminal print chao message
     # https://github.com/wez/wezterm/issues/5007
     enableZshIntegration = false;
