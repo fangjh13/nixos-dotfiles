f3e5ac57ef746f24e669fa05bebfabc296b95af9
Fython <fang.jia.hui123@gmail.com>
2026-05-28 20:34:46 +0800

    refactor(bash): wrap stty configurations in interactive shell check

--- modules/public/homemanager/shell/bash/default.nix
+++ modules/public/homemanager/shell/bash/default.nix
@@ -9,12 +9,16 @@
     bashrcExtra = ''
       export SHELL=bash
 
-      stty werase undef
-      bind '\C-w:unix-filename-rubout'
+      if [[ -t 0 && $- == *i* ]]; then
+        # Disable default TTY word erase and rebind it to path-aware deletion.
+        # This makes Ctrl-W delete backwards up to a slash (/) instead of a space,
+        # which is extremely useful when typing and correcting file paths.
+        stty werase undef
+        bind '\C-w:unix-filename-rubout'
 
-      # disable ctrl-s (freeze terminal)
-      if [[ -t 0 && $- = *i* ]]
-      then
+        # Disable XON/XOFF software flow control.
+        # This prevents the terminal from freezing (pausing output) when Ctrl-S is
+        # pressed accidentally, and frees up Ctrl-S for other readline keybindings.
         stty -ixon
         bind -r "\C-s"
       fi
