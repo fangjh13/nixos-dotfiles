9a736e4e8980a3375953d57fd4a91d89a3f288a0
fangjiahui <fangjh@pmind-tech.com>
2026-05-28 20:37:06 +0800

    build(flake): update dependencies

--- modules/public/homemanager/programming/python/default.nix
+++ modules/public/homemanager/programming/python/default.nix
@@ -3,14 +3,14 @@
   pkgs,
   ...
 }: let
-  llmWithPlugins = pkgs.python313Packages.llm.withPlugins {
+  llmWithPlugins = pkgs.python314Packages.llm.withPlugins {
     # LLM plugin to access Google's Gemini family of models <https://github.com/simonw/llm-gemini>
     llm-gemini = true;
   };
 in {
   # Python
   home.packages = with pkgs; [
-    (python313.withPackages (pyPkgs: with pyPkgs; [requests]))
+    (python314.withPackages (pyPkgs: with pyPkgs; [requests]))
 
     # CLI tool interacting with AI
     llmWithPlugins
