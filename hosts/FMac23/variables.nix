d278547603be794055528f135df64387ca43f1f6
Fython <fang.jia.hui123@gmail.com>
2026-08-01 13:11:15 +0800

    feat(hosts): add configuration for FMac23 host

---
+++ hosts/FMac23/variables.nix
@@ -0,0 +1,32 @@
+{
+  userName = "fython";
+  hostName = "FMac23";
+  gitName = "Fython";
+  gitEmail = " fython.me@gmail.com";
+
+  timezone = "Asia/Shanghai";
+
+  # Extra Homebrew formula packages to be installed
+  brews = [];
+
+  # Extra Homebrew cask packages to be installed
+  casks = [
+    # QEMU Virtual Machines
+    "utm"
+    # Screenshot tools
+    "pixpin"
+    # Docker/k8s management
+    "orbstack"
+    # Cloud storage clients
+    "synology-drive"
+    # Markdown editor
+    "markedit"
+    "obsidian"
+    # Note-taking tools
+    "logseq"
+
+    "antigravity"
+    "antigravity-ide"
+    "codex-app"
+  ];
+}
