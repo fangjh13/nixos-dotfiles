477053e0f6f9617e010d43ec9ba4a85eaa228305
Fython <fython.me@gmail.com>
2026-08-25 00:49:18 +0800

    chore(ultrachao): add age key to sops and configure host options

--- hosts/ultrachao/default.nix
+++ hosts/ultrachao/default.nix
@@ -1,14 +1,13 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-{
-  hostContext,
-  ...
-}: {
+{hostContext, ...}: {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
+
+  addon.mihomo.enable = false;
 
   # NOTE: Enable imported option modules if you need
   drivers.intel.enable = true;
